350583c336d84651b539c7c5cc7289a4b19b837a
SarthakJha-IITMandi <jhasarthak123@gmail.com>
2022-12-11 13:51:43 +0530

    working on setting up structs

--- server.c
+++ server.c
@@ -1,125 +1,84 @@
-/*
-** server.c -- a stream socket server demo
-*/
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
-#define PORT "3490"
-// the port users will be connecting to
-#define BACKLOG 10
-// how many pending connections queue will hold
-void sigchld_handler(int s)
-{
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
-    while (waitpid(-1, NULL, WNOHANG) > 0)
-        ;
-    errno = saved_errno;
-}
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET)
-    {
-        return &(((struct sockaddr_in *)sa)->sin_addr);
-    }
+#include <unistd.h>
 
-    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
-}
-int main(void)
-{
-    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes = 1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
-    {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
-    // loop through all the results and bind to the first we can
-    for (p = servinfo; p != NULL; p = p->ai_next)
-    {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1)
-        {
-            perror("server: socket");
-            continue;
-        }
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                       sizeof(int)) == -1)
-        {
-            perror("setsockopt");
-            exit(1);
-        }
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
-        {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-        break;
-    }
-    freeaddrinfo(servinfo); // all done with this structure
-    if (p == NULL)
-    {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
-    }
-    if (listen(sockfd, BACKLOG) == -1)
-    {
-        perror("listen");
-        exit(1);
-    }
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1)
-    {
-        perror("sigaction");
-        exit(1);
-    }
-    printf("server: waiting for connections...\n");
-    while (1)
-    { // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1)
-        {
-            perror("accept");
-            continue;
-        }
-        inet_ntop(their_addr.ss_family,
-                  get_in_addr((struct sockaddr *)&their_addr),
-                  s, sizeof s);
-        printf("server: got connection from %s\n", s);
+/*
+ * Desired design of maximum size and alignment
+ */
+#define _SS_MAXSIZE 128 /* Implementation specific max size */
+#define _SS_ALIGNSIZE (sizeof(int64_t))
+/* Implementation specific desired alignment */
+/*
+ * Definitions used for sockaddr_storage structure paddings design.
+ */
+#define _SS_PAD1SIZE (_SS_ALIGNSIZE - sizeof(sa_family_t))
+#define _SS_PAD2SIZE (_SS_MAXSIZE - (sizeof(sa_family_t) + _SS_PAD1SIZE + _SS_ALIGNSIZE))
 
-        if (!fork())
-        {                  // this is the child process
-            close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd); // parent doesn't need this
-    }
-    return 0;
-}
+struct addrinfo
+{
+    int ai_flags;             // AI_PASSIVE, AI_CANONNAME, etc.
+    int ai_family;            // AF_INET, AF_INET6, AF_UNSPEC
+    int ai_socktype;          // SOCK_STREAM, SOCK_DGRAM
+    int ai_protocol;          // use 0 for "any"
+    size_t ai_addrlen;        // size of ai_addr in bytes
+    struct sockaddr *ai_addr; // struct sockaddr_in or _in6
+    char *ai_canonname;       // full canonical hostname
+    struct addrinfo *ai_next; // linked list, next node
+};
+
+struct sockaddr
+{
+    unsigned short sa_family; // address family, AF_xxx
+    char sa_data[14];         // 14 bytes of protocol address
+};
+
+// (IPv4 only--see struct sockaddr_in6 for IPv6)
+struct sockaddr_in
+{
+    short int sin_family;        // Address family, AF_INET
+    unsigned short int sin_port; // Port number
+    struct in_addr sin_addr;     // Internet address
+    unsigned char sin_zero[8];   // Same size as struct sockaddr
+};
+
+// (IPv4 only--see struct in6_addr for IPv6)
+struct in_addr
+{
+    uint32_t s_addr; // that's a 32-bit int (4 bytes)
+};
+
+// (IPv6 only--see struct sockaddr_in and struct in_addr for IPv4)
+struct sockaddr_in6
+{
+    u_int16_t sin6_family;     // address family, AF_INET6
+    u_int16_t sin6_port;       // port number, Network Byte Order
+    u_int32_t sin6_flowinfo;   // IPv6 flow information
+    struct in6_addr sin6_addr; // IPv6 address
+    u_int32_t sin6_scope_id;   // Scope ID
+};
+
+// (IPv6 only--see struct sockaddr_in and struct in_addr for IPv4)
+struct in6_addr
+{
+    unsigned char si6_addr[16]; // IPv6 address
+};
+
+// sockaddr_storage is used if ipv4/6 not known in advance
+struct sockaddr_storage
+{
+    sa_family_t ss_family; /* address family */
+    /* Following fields are implementation specific */
+    char _ss_pad1[_SS_PAD1SIZE];
+    /* 6 byte pad, this is to make implementation */
+    /* specific pad up to alignment field that */
+    /* follows explicit in the data structure */
+    int64_t _ss_align; /* field to force desired structure */
+                       /* storage alignment */
+    char _ss_pad2[_SS_PAD2SIZE];
+    /* 112 byte pad to achieve desired size, */
+    /* _SS_MAXSIZE value minus size of ss_family */
+    /* __ss_pad1, __ss_align fields is 112 */
+};
